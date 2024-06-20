e68a7ff4636c3f86e56f99f51e5b5031e1d8c1bd
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-20 13:44:23 +0530

    Two Regular Expression Methods

--- 03-two-regular-expression-methods.js
+++ 03-two-regular-expression-methods.js
@@ -1,8 +1,11 @@
+// https://edabit.com/challenge/grqSjHHQYHBeKK6E3
+
 function twoMethods() {
 	// find and equals are not regular expression methods.  Replace them. 
-	let methodOne = /hello/.find("hello")[0] === "hello" 
-	let methodTwo = /hello/.equals("hello") // returns a boolean
+	// let methodOne = /hello/.find("hello")[0] === "hello" 
+	let methodOne = /hello/.test("hello");
+	let methodTwo = "hello".match(/hello/) // returns a boolean
 	return methodOne && methodTwo? "done": "Not" 
-}
+}	
 
 console.log(twoMethods()) //, true, "done" )
