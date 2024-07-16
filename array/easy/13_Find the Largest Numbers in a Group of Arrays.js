54bcf5fb1fa91bacfa93231dbe01b64821b9830b
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-16 21:46:51 +0530

    Find the Largest Numbers in a Group of Arrays

---
+++ array/easy/13_Find the Largest Numbers in a Group of Arrays.js
@@ -0,0 +1,20 @@
+// https://edabit.com/challenge/h7LTMAFeNz79rXB2Y
+function findLargestNums(arr) {
+	// var a= [];
+let largestNum= arr[i];
+    for(var i=0; i<arr.length; i++){
+    if(arr[i]>largestNum){
+		largestNum = arr[i]
+	}
+	
+}
+
+	return  largestNum
+}
+console.log(findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]])); //[7, 90, 2])
+console.log(findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]])); //[0.7634, 9.32, 9])
+console.log(findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]])); //[-34, -2, 7])
+console.log(findLargestNums([[0.34, -5, 1.34], [-6.432, -1.762, -1.99], [32, 65, -6]])); //[1.34, -1.762, 65])
+console.log(findLargestNums([[0, 0, 0, 0], [3, 3, 3, 3], [-2, -2]])); //[ 0, 3, -2 ])
+
+
