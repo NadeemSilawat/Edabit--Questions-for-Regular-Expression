fc33151f0aaa133e335797fde460e5ef8982d37a
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-16 21:44:44 +0530

    Absolute Sum

---
+++ array/easy/11_Absolute Sum.js
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/rCmEy2AQYLbRGgKyL
+function getAbsSum(arr) {
+	return arr=Math.abs()
+}
+console.log(getAbsSum([2, -1, -3, 4, 8])); //18);
+console.log(getAbsSum([-1])); //1);
+console.log(getAbsSum([-1, -3, -5, -4, -10, 0])); //23);
+console.log(getAbsSum([8, 9, 10, 32, 101, -10])); //170);
+console.log(getAbsSum([500])); //500);
