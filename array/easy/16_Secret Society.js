7c5b0d47b00390a629c241e9fbe3e18738618332
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-18 22:59:07 +0530

    Secert Society

---
+++ array/easy/16_Secret Society.js
@@ -0,0 +1,14 @@
+// function societyName(friends) {
+//     //  function compareNumbers(a, b) {
+//     //     return a.length - b.length;
+//     //   }     
+//     return friends.sort().map(word => word[0]).join('');
+// 	debugger
+// }
+// console.log(societyName(['Adam', 'Sarah', 'Malcolm'])); //'AMS')
+// console.log(societyName(['Phoebe', 'Chandler', 'Rachel', 'Ross', 'Monica', 'Joey'])); //'CJMPRR')
+// console.log(societyName(['Harry', 'Newt', 'Luna', 'Cho'])); //'CHLN')
+// console.log(societyName(['Sherlock', 'Irene', 'John'])); //'IJS')
+// console.log(societyName(['Sheldon', 'Amy', 'Penny', 'Howard', 'Raj'])); //'AHPRS')
+
+
