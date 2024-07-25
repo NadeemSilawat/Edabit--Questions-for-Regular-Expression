2cf199bc4885c9766fcf0f532038342ee3c3c3cd
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-25 22:56:34 +0530

    Convert a Number

---
+++ Number/easy/04_Convert a Number to Base-2.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/3kcrnpHk7krNZdnKK
+function binary(decimal) {
+	return decimal.toString(2);
+}   
+console.log(binary(100)); //"1100100")
+console.log(binary(1)); //"1")
+console.log(binary(0)); // "0")
+console.log(binary(69)); //"1000101")
+console.log(binary(1023)); //"1111111111")
+console.log(binary(511)); //"111111111")
+console.log(binary(666)) //"1010011010")
+console.log(binary(123)); //"1111011")
