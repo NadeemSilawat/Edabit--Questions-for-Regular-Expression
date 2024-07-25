3f6ff8ebbcab95305f4f66eb4b310141cbe1e473
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-25 22:57:55 +0530

    Return the Sum of Two Numbber

---
+++ Number/very easy/01_Return the Sum of Two Numbers.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+function addition(a, b) {
+    return a+=b
+}
+console.log(addition(2, 3)); //5)
+console.log(addition(-3, -6)); //-9)
+console.log(addition(7, 3)); //10)
+console.log(addition(88, 2)); //90)
