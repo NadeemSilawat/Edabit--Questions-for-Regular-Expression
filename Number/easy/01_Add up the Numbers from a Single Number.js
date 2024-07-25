dfb6737bb583a54f33055580dc69fd9f2d4bfe50
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-25 22:53:48 +0530

    Add up the Number from a Single Number

---
+++ Number/easy/01_Add up the Numbers from a Single Number.js
@@ -0,0 +1,53 @@
+https://edabit.com/challenge/4gzDuDkompAqujpRi
+function addUp(num) {
+    //Answer 1
+    // let sum = 0 ;
+	// for(var i=0; i<=num; i++){
+    //    sum += i;
+    // }
+    // return sum
+
+    //Answer 2
+}
+console.log(addUp(4)); //10)
+console.log(addUp(13)); //91)
+console.log(addUp(600)); //180300)
+console.log(addUp(392)); //77028)
+console.log(addUp(53)); //1431)
+console.log(addUp(897));  //402753)
+console.log(addUp(23)); //276)
+console.log(addUp(1000)); //500500)
+console.log(addUp(738)); //272691)
+console.log(addUp(100)); //5050)
+console.log(addUp(925)); // 428275)
+console.log(addUp(1)); //1)
+console.log(addUp(999)); //499500)
+console.log(addUp(175)); //15400)
+console.log(addUp(111)); //6216)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // return (num*(num + 1))/2
