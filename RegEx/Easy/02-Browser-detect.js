e08526ce4cdd684dfae02cdf876a1bd8e59ae771
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-25 22:45:31 +0530

    Browser Detect

---
+++ RegEx/Easy/02-Browser-detect.js
@@ -0,0 +1,14 @@
+function detectBrowser(userAgent) {
+
+const  browserList= {
+    Chrome: "Google Chrome",
+    Fir
+}
+
+	// var r = userAgent.match(/Internet/g)
+
+    // return userAgent.match(/Internet/g)?'Google Chrome': "Does not Match"
+;}
+	 
+console.log(detectBrowser('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0')) 
+console.log(detectBrowser('Microsoft Internet Explorer/4.0b1 (Windows 95)'))
