5af3fc6de365d492718184f8cd4aa248cbf6915b
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-07-30 11:34:10 +0530

    Destructuring Array

---
+++ array/very easy/06_ES6 Destructuring Arrays I.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5, 6]
+// Pattern 1
+// a = arr[0];
+// b = arr[1];
+
+//Pattern 2
+const [a, b] = arr
+
+console.log((a)); // true, "Variable a does not equal 1")
+console.log((b)); // true, "Variable b does not equal 2")
