4135713586a7d6ea465dc05acc408a21b44e603b
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-08-02 17:40:12 +0530

    Boolean to String Conversion

---
+++ String/very easy/04_Boolean to String Conversion.js
@@ -0,0 +1,9 @@
+function boolToString(flag) {
+	//Answer 1
+	// return flag.toString()
+
+	//Answer2
+	return flag? 'true' : 'false';
+}
+console.log(boolToString(true)); //"true")
+console.log(boolToString(false)); //"false")
