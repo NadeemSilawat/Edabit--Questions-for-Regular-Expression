8c0834509c6aa05939b867a6dea8d1a19e1e5d8b
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-08-02 17:38:44 +0530

    Return Something to Me

---
+++ String/very easy/02_Return Something to Me!.js
@@ -0,0 +1,12 @@
+function giveMeSomething(a) {
+    //Answer 1
+    let hello= "something"
+    let result = hello + a
+	return result
+
+    //Answer2
+    
+}
+console.log(giveMeSomething("a")); //"something a")
+console.log(giveMeSomething("is cooking")); //"something is cooking")
+console.log(giveMeSomething(" is cooking")); //"something  is cooking")
