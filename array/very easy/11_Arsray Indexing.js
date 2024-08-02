c8ec21e38fa992541ec3096c0f11baef25172227
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-08-02 17:35:07 +0530

    Array Indexing

---
+++ array/very easy/11_Arsray Indexing.js
@@ -0,0 +1,9 @@
+function valueAt(arr, i) {
+    var a =  Math.floor(i);
+  
+       return arr[a];
+  }
+  console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2));//6)
+  console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2)); //4)
+  console.log(valueAt([1, 2], 1.0 / 2)); // 1)
+  console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2));  //5)
