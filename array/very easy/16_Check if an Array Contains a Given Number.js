84223ad17f177c400ebdb2c4448a757dcfb1dc0f
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-08-04 21:16:32 +0530

    Check is an Array Contains a Given Number

---
+++ array/very easy/16_Check if an Array Contains a Given Number.js
@@ -0,0 +1,18 @@
+function check(arr, el) {
+    //pettern 1
+    // return arr.includes(el)
+
+    //pettern 2
+    for (var i = 0; i < arr.length; i++){
+		if (arr[i] == el){
+			return true;
+		}
+        return false;
+	}
+}
+console.log(check([1, 2, 3, 4, 5], 3)); //true)
+console.log(check([1, 1, 2, 1, 1], 3)); //false)
+console.log(check([1, 1, 2, 1, 5, 4, 7], 7)); //true)
+console.log(check([1, 1, 2, 1, 5, 4, 7], 8)); //false)
+console.log(check([5, 5, 5, 6], 5)); //true)
+console.log(check([], 5)); //false)
