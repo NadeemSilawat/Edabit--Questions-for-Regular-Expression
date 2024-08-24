3dc01dc16f35722ba915125c9bdf066965798952
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-08-24 09:14:19 +0530

    Array of String to Array of Numbers

---
+++ array/very easy/20_Array of Strings to Array of Numbers.js
@@ -0,0 +1,15 @@
+function toNumberArray(arr) {
+	return arr.map(Number);
+}
+console.log(toNumberArray(["1", "2"])); //[1, 2])
+console.log(toNumberArray(["3", "4"])); //[3, 4])
+console.log(toNumberArray(["5", "6"])); // [5, 6])
+console.log(toNumberArray(["7", "8"])); //[7, 8])
+console.log(toNumberArray(["9", "10"])); //[9, 10])
+console.log(toNumberArray(["91", "44"])); //[91, 44])
+console.log(toNumberArray(["19", "14"])); //[19, 14])
+console.log(toNumberArray(["29", "34"])); //[29, 34])
+console.log(toNumberArray(["0", "4"])); //[0, 4])
+console.log(toNumberArray(["21", "23"])); //[21, 23])
+console.log(toNumberArray(["12", "25"])); //[12, 25])
+console.log(toNumberArray(["99", "20"]));// [99, 20])
