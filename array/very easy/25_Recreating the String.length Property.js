8a1f6b9ac3b9e515d0101e9bb797575021f396c3
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-10-01 07:50:27 +0530

    Recreating the String length property

---
+++ array/very easy/25_Recreating the String.length Property.js
@@ -0,0 +1,17 @@
+function length(s) {
+    //pettern 1
+    return length(s);
+
+    //pettern 2
+    // for(var i=s.length-1;i<=0; i++){
+        
+    // }
+    // return s 
+}
+console.log(length("Hello World")); //11)
+console.log(length("Edabit")); //6)
+console.log(length("wash your hands!")); //16)
+console.log(length("34445")); //5)
+console.log(length("   ")); //3)
+console.log(length("")); //0)
+console.log(length("%%$")); //3)
