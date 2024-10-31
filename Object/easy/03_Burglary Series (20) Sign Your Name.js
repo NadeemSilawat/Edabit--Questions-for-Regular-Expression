14f595afdf59ebe9a05a5df53ea5302cc59ddbdd
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-10-31 15:27:11 +0530

    Burglary Series

---
+++ Object/easy/03_Burglary Series (20) Sign Your Name.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/xRcgQHtfLbxomYb33
+
+
+function signYourName(obj) {
+    // write your code here
+      // don't use a return statement
+      
+      
+    // DON'T CHANGE OR REMOVE THE LINES BELOW
+    obj.yourSignature = "Whatever";
+      obj.spouseSignature != "Nice Try"
+    return obj;
+  }
+  
+  console.log(signYourName({yourSignature: ""})); //{yourSignature: "Whatever"})
