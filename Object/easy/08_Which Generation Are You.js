a50a2f2a00a70b3e729b714ffbe4dd4ebc0fe960
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-10-31 15:31:02 +0530

    Which Generation are you

---
+++ Object/easy/08_Which Generation Are You.js
@@ -0,0 +1,22 @@
+// https://edabit.com/challenge/48EJWLhF224na8po3
+
+function generation(x, y) {
+	
+    return 
+}
+
+console.log(generation(-3, "m")); //"great grandfather", "3 generations before you, male...")
+console.log(generation(1, "f")); //"daughter", "1 generation after you, female...")
+console.log(generation(-3, "f"));  //"great grandmother")
+console.log(generation(-2, "m")); //"grandfather")
+console.log(generation(-2, "f")); //"grandmother")
+console.log(generation(-1, "m")); //"father")
+console.log(generation(-1, "f")); //"mother")
+console.log(generation(0, "f")); //"me!")
+console.log(generation(1, "m")); //"son")
+console.log(generation(1, "f")); ///"daughter")
+console.log(generation(2, "m")); //"grandson")
+console.log(generation(2, "f")); //"granddaughter")
+console.log(generation(3, "m")); //"great grandson")
+console.log(generation(3, "f")); //"great granddaughter")
+console.log(generation(0, "m")); //"me!")
